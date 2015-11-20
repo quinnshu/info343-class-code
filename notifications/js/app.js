176881e6e6abfc4dea4c5500c6d681edabd227c7
quinnshu <quinnshu@gmail.com>
2015-11-19 17:05:39 -0800

    11 19 class

--- notifications/js/app.js
+++ notifications/js/app.js
@@ -3,4 +3,37 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+
+    function askPermission() {
+        Notification.requestPermission(function(result) {
+            if('granted' === result) {
+            showNotification('Thanks!', 'You will now recieve notifications from this website');
+        }
+
+        });
+    }
+
+
+    function showNotification (title, body) {
+        var note = new Notification(title, {body: body, icon: 'img/notification.png'});
+        window.setTimeout(note.close.bind(note), 3000);
+
+    }
+
+    var triggerBtn = document.getElementById('trigger');
+    triggerBtn.addEventListener('click', function() {
+        switch (Notification.permission) {
+            case 'granted':
+                showNotification('Hello', 'triggered at ' + new Date().toLocaleTimeString());
+                break;
+
+            case 'denied':
+                alert('Please enable notifications!');
+                break;
+
+            default:
+                askPermission();jak
+        }
+    });
+
 });
